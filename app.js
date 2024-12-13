adcd7d776769ae12b056f51a3d4cd90531068f0d
Connoroni <mitchell2k2@gmail.com>
2024-12-13 14:19:17 +0000

    set up array of objects, created thumbnail function but haven't got it working yet

---
+++ app.js
@@ -0,0 +1,62 @@
+const thumbs = [
+  {
+    src: "https://cdn.pixabay.com/photo/2019/10/13/20/28/gorilla-4547188_1280.jpg",
+    alt: "A silverback gorilla standing on grass on its hands and feet.",
+    width: "4rem",
+  },
+  {
+    src: "unsplash-sikkema-gorilla-closeup.jpg",
+    alt: "A closeup of a gorilla with its eyes looking upwards.",
+    width: "4rem",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2018/05/04/12/13/gorilla-3373856_1280.jpg",
+    alt: "A silverback gorilla laying on a sack and inspecting some grass.",
+    width: "4rem",
+  },
+  {
+    src: "unsplash-ungaro-gorilla-leaves.jpg",
+    alt: "A fully black gorilla facing to the left. It is surrounded by many plants with many green leaves.",
+    width: "4rem",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2020/10/26/08/31/gorilla-5686652_1280.jpg",
+    alt: "A gorilla sitting on a rock chewing on what looks like a carrot.",
+    width: "4rem",
+    height: "4rem",
+  },
+  {
+    src: "https://images5.alphacoders.com/416/416389.jpg",
+    alt: "A silverback gorilla sat on a rock on a grassy hill. He is holding a stick and looking curiously towards the camera.",
+    width: "4rem",
+    height: "4rem",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2018/06/22/03/32/gorilla-3489893_1280.jpg",
+    alt: "A mother gorilla sits watching her child, who is sat next to her.",
+    width: "4rem",
+    height: "4rem",
+  },
+  {
+    src: "https://cdn.pixabay.com/photo/2022/05/21/20/43/young-gorilla-7212279_1280.jpg",
+    alt: "An infant gorilla sitting on a log. It is chewing on a plant.",
+    width: "4rem",
+    height: "4rem",
+  },
+];
+
+function createThumbs(thumbsArray) {
+  thumbContainer = document.getElementById(thumb - container);
+  for (let i = 0; i < thumbsArray.length; i++) {
+    let image = document.createElement("img");
+    image.src = thumbsArray[i].src;
+    image.alt = thumbsArray[i].alt;
+    image.width = thumbsArray[i].width;
+    image.height = thumbsArray[i].height;
+    thumbContainer.appendChild(image);
+  }
+}
+
+createThumbs(thumbs);
+
+// function createBigImageHandler(bigImage) {}
